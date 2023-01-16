b868c8e5183c5e500493117eb977fdf77f064698
Cerecero <ale.rey.cer@outlook.com>
2023-01-16 15:26:25 -0600

    added about, interest and footer sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Info from './components/Info';
-
+import About from './components/About';
+import Interests from './components/Interests';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="App">
       <div class="main-info">
         <Info />
+        <div className="about-interest">
+        <About />
+        <Interests />
+        </div>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interests() {
+    return (
+        <div className="interest">
+            <h1 className="h1-interest">Interests</h1>
+            <p className="p-interest">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. ENtrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    );
+}
