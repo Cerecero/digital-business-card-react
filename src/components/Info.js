ce83552e53a9a42c81d76d48685e2fb741644ed5
Cerecero <ale.rey.cer@outlook.com>
2023-01-16 16:39:58 -0600

    added styles to the footer, about and interest sections

--- src/components/Info.js
+++ src/components/Info.js
@@ -9,10 +9,16 @@ export default function Info() {
             <img src={portraitPlaceholder} alt="placeholder" className="portrait"/>
             <h1 className="name">Alejandro Reyes</h1>
             <p className="title">Fullstack Developer</p>
-            <p className="github-link">Github Page here</p>
+            <p className="github-link">https://github.com/Cerecero</p>
             <div className="info-buttons">
-                <button className="button email"><FaEnvelope /> E-mail</button>
-                <button className="button linkedin"><FaLinkedin /> Linkedin</button>
+                {/* <form href="#" target="_blank"  action="https://stackoverflow.com/questions/57234823/link-in-react-doesnt-forward-to-a-new-page"> */}
+                    <a href='mailto:ale.rey.cer@gmail.com' type='button'>
+                        <button className="button email" ><FaEnvelope /> E-mail</button>
+                    </a>
+                {/* </form> */}
+                <form href="#" target="_blank"  action="https://www.linkedin.com/in/alejandro-reyes-cerecero/">
+                    <button className="button linkedin"><FaLinkedin /> Linkedin</button>
+                </form>
             </div>
         </header>
     );
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests() {
     return (
         <div className="interest">
             <h1 className="h1-interest">Interests</h1>
-            <p className="p-interest">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. ENtrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="p-interest">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepeneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
     );
 }
