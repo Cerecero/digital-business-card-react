8751c7f1ee9a5d72762f12765f92441f4fd0a8a0
Cerecero <ale.rey.cer@outlook.com>
2023-01-16 18:01:02 -0600

    modified the text

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="about">
             <h1 className="h1-about">About</h1>
-            <p className="p-about">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="p-about">I am a fullstack developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
         </div>
     );
 }
